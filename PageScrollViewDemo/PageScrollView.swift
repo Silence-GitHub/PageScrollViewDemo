847eaf3772826652718f69a9f95f2f7f477be1b5
Kaibo <lukaibolkb@163.com>
2017-03-08 13:16:19 +0800

    First commit

---
+++ PageScrollViewDemo/PageScrollView.swift
@@ -0,0 +1,51 @@
+//
+//  PageScrollView.swift
+//  PageScrollViewDemo
+//
+//  Created by Kaibo Lu on 2017/3/7.
+//  Copyright © 2017年 Kaibo Lu. All rights reserved.
+//
+
+import UIKit
+
+class PageScrollView: UIScrollView, UIScrollViewDelegate {
+    
+    var interactionAreaNotInBounds: [CGRect] = [] // Use bounds coordinate system
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupView() {
+        clipsToBounds = false
+        isPagingEnabled = true
+    }
+    
+    override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+        print(#function, "poiont", point)
+        let bounds = self.bounds
+        let areas = interactionAreaNotInBounds.map { (rect) -> CGRect in
+            return CGRect(x: bounds.minX + rect.minX,
+                          y: bounds.minY + rect.minY,
+                          width: rect.width,
+                          height: rect.height)
+        }
+        for area in areas where area.contains(point) {
+            for subview in subviews {
+                let convertedPoint = convert(point, to: subview)
+                if let view = subview.hitTest(convertedPoint, with: event) {
+                    print(#function, "converted point", convertedPoint)
+                    return view
+                }
+            }
+            return self
+        }
+        return super.hitTest(point, with: event)
+    }
+
+}
--- PageScrollViewDemo/ViewController.swift
+++ PageScrollViewDemo/ViewController.swift
@@ -10,16 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var scrollView: PageScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        scrollView = PageScrollView(frame: CGRect(x: 30, y: 0, width: view.bounds.width - 60, height: 300))
+        let width = scrollView.bounds.width
+        let height = scrollView.bounds.height
+        scrollView.interactionAreaNotInBounds = [CGRect(x: scrollView.bounds.minX - 30,
+                                                        y: scrollView.bounds.minY,
+                                                        width: 30,
+                                                        height: height),
+                                                 CGRect(x: scrollView.bounds.maxX,
+                                                        y: scrollView.bounds.minY,
+                                                        width: 30,
+                                                        height: height)]
+        scrollView.contentSize = CGSize(width: width * 3, height: height)
+        
+        for i in 0..<3 {
+            let cell = UIView(frame: CGRect(x: CGFloat(i) * width, y: 0, width: width, height: height))
+            scrollView.addSubview(cell)
+            
+            switch i {
+            case 0:
+                cell.backgroundColor = .red
+            case 1:
+                cell.backgroundColor = .green
+            default:
+                cell.backgroundColor = .blue
+            }
+        }
+        view.addSubview(scrollView)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
